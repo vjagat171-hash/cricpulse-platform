fd8ffc04ecd2edf9b57c3d7a04bfbda747425774
anishav473 <anishav473@gmail.com>
2026-03-25 10:22:30 +0530

    changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/cricpulse-platform/",   // 👈 repo-name add karo
   plugins: [react()],
 })
